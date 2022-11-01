f5c719292663422f0bfe4ac5e94004aac7c540dc
DorianSobanski <tojadorian@gmail.com>
2022-11-01 23:02:18 +0100

    v1

---
+++ index.js
@@ -0,0 +1,40 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client({intents: 771});
+
+client.once('ready', () =>
+{
+    console.log("Corpus Bot jest online");
+    
+    client.user.setActivity("Szanty", {type: 2});
+
+    let guild = client.guilds.cache.get('353233336405065731');
+
+    function countMembers()
+    {
+        client.channels.cache.get('1037099570020241439').setName(`👥》Jest ${guild.memberCount} piratów!`);
+        client.channels.cache.get('1037099673179127859').setName(`🟢》 ${guild.members.cache.filter(m => m.presence?.status == 'online' || m.presence?.status == 'idle' || m.presence?.status == 'dnd').size}`);
+        client.channels.cache.get('1037099702065299476').setName(`🔴》 ${guild.memberCount - guild.members.cache.filter(m => m.presence?.status == 'online' || m.presence?.status == 'idle' || m.presence?.status == 'dnd').size}`);
+        
+        console.log(`Status przeładowany: ${guild.memberCount} łącznie, ${guild.memberCount - guild.members.cache.filter(m => m.presence?.status == 'online' || m.presence?.status == 'idle' || m.presence?.status == 'dnd').size} offline, ${guild.members.cache.filter(m => m.presence?.status == 'online' || m.presence?.status == 'idle' || m.presence?.status == 'dnd').size} online`);
+    } countMembers();
+
+    function countDate(){
+        var today = new Date();
+        var start = new Date('09/05/2019');
+        var diff = ((((Math.abs(today - start) / 1000) / 60) / 60) / 24);
+        var days = String(Math.floor(diff)); 
+
+        client.channels.cache.get('771733587962626058').setName(`🌞》To już ${days} dni!`);
+
+        console.log(`Różnica dat przeładowana: ${days} dni`)
+    } countDate()
+
+    setInterval(() => 
+    {
+        countMembers();
+        countDate();
+    }, 60000);
+});
+
+client.login('MTAzNjk4NTQ1Mjg4NTk3OTE0Nw.GPuXpV.MRqR2ardd9IM2K_ZXTGRbgLdk4piFa_pDwK4aA');
